3efaf6062fbea431e50884ec7954bcaba8682403
Jerry <Jerry>
2017-02-10 19:34:08 +0800

    新增top

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+
+var s = skrollr.init();
+
+$(document).ready(function() {
+	$(".totop").click(function(event) {
+			$("html,body").animate({scrollTop : 0}, 300);
+	});
+
+});
+
 $(window).scroll(function(){
   if($(window).scrollTop()>0){
     $(".navbar").removeClass("navbar-top");
@@ -6,4 +16,3 @@ $(window).scroll(function(){
   }
 });
 
-var s = skrollr.init();
