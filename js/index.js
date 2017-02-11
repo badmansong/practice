f940ac77d40966625c90afaff35fc8d4e19101d2
Jerry <Jerry>
2017-02-11 23:11:56 +0800

    手機版調整

--- js/index.js
+++ js/index.js
@@ -2,17 +2,9 @@
 var s = skrollr.init();
 
 $(document).ready(function() {
+	//置頂按鈕
 	$(".totop").click(function(event) {
-			$("html,body").animate({scrollTop : 0}, 300);
+	$("html,body").animate({scrollTop : 0}, 300);
 	});
 
 });
-
-$(window).scroll(function(){
-  if($(window).scrollTop()>0){
-    $(".navbar").removeClass("navbar-top");
-  }else{
-    $(".navbar").addClass("navbar-top");
-  }
-});
-
