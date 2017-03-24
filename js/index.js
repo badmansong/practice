e37574e05d0ac7488f365141552d73af10b3a170
badmansong <badmansong@hotmail.com>
2017-03-24 19:48:00 +0800

    滑動卷軸

--- js/index.js
+++ js/index.js
@@ -7,4 +7,26 @@ $(document).ready(function() {
 	$("html,body").animate({scrollTop : 0}, 300);
 	});
 
+	//滾動目標
+	$("a.move").on("click",function(){
+		var getHref = $(this).attr("href");
+		
+		var getHrefTop = $(getHref).offset().top;
+		console.log(getHrefTop);
+		$("body").animate({scrollTop:getHrefTop},300);
+		return false;
+
+	})
+	$(window).on("scroll" , function(){
+		var scroll = $(window).scrollTop();
+
+		if(scroll>300){
+			$(".totop").fadeIn();
+		}else{
+			$(".totop").fadeOut();
+		}
+		// console.log(scroll)
+
+
+	})
 });
