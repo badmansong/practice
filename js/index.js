f2f88b8e2099bd2c514c02b058d72b9d62029a64
badmansong <badmansong@hotmail.com>
2017-04-30 23:42:41 +0800

    首頁滾動卷軸

--- js/index.js
+++ js/index.js
@@ -3,8 +3,22 @@ var s = skrollr.init();
 
 $(document).ready(function() {
 	//置頂按鈕
-	$(".totop").click(function(event) {
+	$(window).on('scroll',function(){
+	if($(window).scrollTop()>100){
+	$('.totop').fadeIn();
+	}else{
+		$('.totop').fadeOut();
+	}
+		
+	})
+	$(".totop").click(function() {
 	$("html,body").animate({scrollTop : 0}, 300);
 	});
-
+	//緩移到目標
+	$('.TAG').on('click',function(){
+	var target = $(this).attr('href')
+		$('html,body').animate({scrollTop:$(target).offset().top})
+		return false;
+	})
+	
 });
